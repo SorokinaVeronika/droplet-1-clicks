c494e8a9a00be7ef7734c4c6ba47ade69e813fe1
SorokinaVeronika <sorokina.veronika91@gmail.com>
2025-03-17 11:48:50 -0700

    Update 011-docker-compose.sh

--- common/scripts/011-docker-compose.sh
+++ common/scripts/011-docker-compose.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-mkdir -p ~/.docker/cli-plugins/;
 if [ -z "${docker_compose_version}" ]; then
   docker_compose_version=$(curl -sSL "https://api.github.com/repos/docker/compose/releases/latest" | jq -r '.tag_name');
 fi
-curl -SL https://github.com/docker/compose/releases/download/${docker_compose_version}/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose;
-chmod +x ~/.docker/cli-plugins/docker-compose;
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker};
+mkdir -p $DOCKER_CONFIG/cli-plugins;
+curl -SL https://github.com/docker/compose/releases/download/v2.33.1/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose;
+chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
